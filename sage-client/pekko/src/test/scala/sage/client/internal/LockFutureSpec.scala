e6a20ab364011961a50c42ee6519a35d2b044bac
Pierre Ricadat <ghostdogpr@gmail.com>
2026-09-05 08:20:37 +0900

    Remove timing-dependent Pekko lock assertion

--- sage-client/pekko/src/test/scala/sage/client/internal/LockFutureSpec.scala
+++ sage-client/pekko/src/test/scala/sage/client/internal/LockFutureSpec.scala
@@ -1,6 +1,6 @@
 package sage.client.internal
 
-import java.util.concurrent.atomic.{AtomicBoolean, AtomicInteger}
+import java.util.concurrent.atomic.AtomicBoolean
 
 import scala.concurrent.{ExecutionContext, Promise}
 import scala.concurrent.duration.*
@@ -62,16 +62,14 @@ class LockFutureSpec extends munit.FunSuite {
     checked.andThen { case _ => live.close.unsafeRun }
   }
 
-  test("lease loss stops renewal while a Future body keeps running") {
+  test("lease loss fails the scope while a Future body keeps running") {
     val body      = Promise[Int]()
     val continued = Promise[Int]()
-    val renewals  = new AtomicInteger(0)
     val released  = new AtomicBoolean(false)
     val commands  = new CommandRunner[CIO, String] {
       def run[A](command: Command[A]): CIO[A] = CIO.defer(()).flatMap { _ =>
         val reply = command.args(4).asUtf8String match {
           case "renew"   =>
-            renewals.incrementAndGet()
             0L
           case "release" =>
             released.set(true)
@@ -93,9 +91,6 @@ class LockFutureSpec extends munit.FunSuite {
       _      = assert(error.isInstanceOf[LockLost], error.toString)
       _      = assert(!continued.isCompleted)
       _      = assert(released.get())
-      count  = renewals.get()
-      _     <- CIO.sleep(400.millis).unsafeRun
-      _      = assertEquals(renewals.get(), count)
       _      = body.success(42)
       value <- continued.future
     } yield assertEquals(value, 42)
